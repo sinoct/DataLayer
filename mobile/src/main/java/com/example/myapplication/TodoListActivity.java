7d96197d560263dde7e446a1132fe4b1f2b64134
Geristen <sanyibacsi19@gmail.com>
2020-10-10 14:36:38 +0300

    Replaced the TextView in the TodoListActivity with a ListView, tapping on the items in the list makes the background green, meaning that the task has been completed.

--- mobile/src/main/java/com/example/myapplication/TodoListActivity.java
+++ mobile/src/main/java/com/example/myapplication/TodoListActivity.java
@@ -1,23 +1,38 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TodoListActivity extends MainActivity {
 
-    TextView todoListView;
+    ListView todoListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todolist_layout);
         todoListView = findViewById(R.id.todoListView);
-        String list = "";
+        ArrayList<String> lista =  new ArrayList<String>();
         for (String item : MainActivity.todoList) {
-            list = list + item + "\n";
+            lista.add(item);
         }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.simple_text,lista);
+        todoListView.setAdapter(adapter);
 
-        todoListView.setText(list);
+        todoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setBackgroundColor(getColor(R.color.selected));
+            }
+        });
 
     }
 }
--- wear/src/main/java/com/example/myapplication/TodoListActivity.java
+++ wear/src/main/java/com/example/myapplication/TodoListActivity.java
@@ -1,26 +1,39 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class TodoListActivity extends MainActivity{
 
-    TextView todoListView;
+    ListView todoListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todolist_layout);
-        todoListView = (TextView) findViewById(R.id.todoListView);
-        String list = "";
-        for (String item: MainActivity.todoList) {
-            list = list + item + "\n";
+        todoListView = findViewById(R.id.todoListView);
+        final ArrayList<String> lista =  new ArrayList<String>();
+        for (String item : MainActivity.todoList) {
+            lista.add(item);
         }
-        todoListView.setText(list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.simple_text,lista);
+        todoListView.setAdapter(adapter);
+
+        todoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setBackgroundColor(Color.parseColor("#32ad09"));
+            }
+        });
 
     }
 }
